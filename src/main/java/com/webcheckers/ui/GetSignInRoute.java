86ca824619745d96231931fb7f0795ac02e5d159
rock <bb5369@g.rit.edu>
2020-09-29 19:37:06 -0400

    learning spark: test2

--- src/main/java/com/webcheckers/ui/GetSignInRoute.java
+++ src/main/java/com/webcheckers/ui/GetSignInRoute.java
@@ -3,7 +3,9 @@ package com.webcheckers.ui;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.logging.Logger;
 
+import com.webcheckers.util.Message;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -20,12 +22,46 @@ import static spark.Spark.halt;
  * @author <a href='mailto:jrv@se.rit.edu'>Jim Vallino</a>
  */
 public class GetSignInRoute implements Route {
+    private static final Logger LOG = Logger.getLogger(GetHomeRoute.class.getName());
 
-    get(HOME_URL, new GetHomeRoute(templateEngine)
+    private static final Message WELCOME_MSG = Message.info("Welcome to the world of online Checkers.");
 
+    private final TemplateEngine templateEngine;
+
+    /**
+     * Create the Spark Route (UI controller) to handle all {@code GET /} HTTP requests.
+     *
+     * @param templateEngine
+     *   the HTML template rendering engine
+     */
+    public GetSignInRoute(final TemplateEngine templateEngine) {
+        this.templateEngine = Objects.requireNonNull(templateEngine, "templateEngine is required");
+        //
+        LOG.config("GetSignInRoute is initialized.");
+    }
+
+    /**
+     * Render the WebCheckers Home page.
+     *
+     * @param request
+     *   the HTTP request
+     * @param response
+     *   the HTTP response
+     *
+     * @return
+     *   the rendered HTML for the Home page
+     */
     @Override
-    public Object handle(Request request, Response response) throws Exception {
-        return null;
-    });
+    public Object handle(Request request, Response response) {
+        LOG.finer("GetHomeRoute is invoked.");
+        //
+        Map<String, Object> vm = new HashMap<>();
+        vm.put("title", "Welcome!");
 
+        // display a user message in the Home page
+        vm.put("message", WELCOME_MSG);
+
+        // render the View
+        return templateEngine.render(new ModelAndView(vm , "home.ftl"));
+    }
 }
--- src/main/java/com/webcheckers/ui/WebServer.java
+++ src/main/java/com/webcheckers/ui/WebServer.java
@@ -53,7 +53,7 @@ public class WebServer {
    * The URL pattern to request the Home page.
    */
   public static final String HOME_URL = "/";
-  public static final String SIGNIN_URL = "/signin";
+  public static final String SIGNIN_URL = "signin";
 
   //
   // Attributes
@@ -138,8 +138,8 @@ public class WebServer {
     //// code clean; using small classes.
 
     // Shows the Checkers game Home page.
-    //get(HOME_URL, new GetHomeRoute(templateEngine));
-    get(SIGNIN_URL, new GetHomeRoute(templateEngine));
+    get(HOME_URL, new GetHomeRoute(templateEngine));
+    get(SIGNIN_URL, new GetSignInRoute(templateEngine));
 
     //
     LOG.config("WebServer is initialized.");
