ae8ec96a3550602e64aaced8af182214cb21a3a0
Gideon Wikina <widdy49@gmail.com>
2020-10-01 08:09:35 -0400

    Add model components and game center

--- src/main/java/com/webcheckers/model/Piece.java
+++ src/main/java/com/webcheckers/model/Piece.java
@@ -1,12 +1,6 @@
 package com.webcheckers.model;
 
-/**
- * A single checkers piece
- * Can be a single piece or a king
- * Can be red or white
- *
- * @author Ries Scerbin
- */
+
 
 public class Piece {
     //The type of the Piece
@@ -18,27 +12,16 @@ public class Piece {
     //Color of chest piece.
     public enum Color{ RED, WHITE}
 
-    /**
-     *Constructor for a piece
-     *
-     * @param type The type of the piece
-     * @param color The color of the piece
-     */
+
     public Piece(Type type, Color color){
         this.type = type;
         this.color = color;
     }
 
-    /**
-     *Gets the type of the piece
-     * @return Return the type of the piece
-     */
+
     public Type getType(){ return this.type; }
 
-    /**
-     *Gets the color of the piece
-     * @return Returns the color of the piece
-     */
+
     public Color getColor(){
         return this.color;
     }
