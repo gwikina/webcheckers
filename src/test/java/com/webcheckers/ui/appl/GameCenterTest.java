dbd7f35d3b84fbaa7c19e372b252fc55d0ca5900
Heather Moses <hlm8500@rit.edu>
2020-10-12 20:40:47 -0400

    Adding GameCenterTest

--- src/test/java/com/webcheckers/ui/appl/GameCenterTest.java
+++ src/test/java/com/webcheckers/ui/appl/GameCenterTest.java
@@ -1,4 +1,39 @@
 package com.webcheckers.ui.appl;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.webcheckers.appl.GameCenter;
+import com.webcheckers.appl.PlayerLobby;
+import static org.mockito.Mockito.mock;
+
+import com.webcheckers.model.Player;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import com.webcheckers.model.Game;
+
+/**
+ * The unit test suite for the {@link GameCenter} component.
+ *
+ */
+@Tag("Application-tier")
 public class GameCenterTest {
+
+    /**
+     * Test the ability to make a new Game.
+     */
+    @Test
+    public void test_make_game() {
+        final GameCenter CuT = new GameCenter();
+        // Invoke test
+        // Friendly Player1
+        final Player player1 = new Player("Rocky");
+        // Friendly Player2
+        final Player player2 = new Player("Ella");
+
+        final Game game = CuT.makeGame(player1, player2);
+        // Analyze results
+        assertNotNull(game);
+    }
+
 }
