e1155020ffb7bf0e398608764091eda3037d909e
Gideon Wikina <widdy49@gmail.com>
2020-10-13 01:46:17 -0400

    Added PlayerLobbyTest

--- src/test/java/com/webcheckers/ui/appl/PlayerLobbyTest.java
+++ src/test/java/com/webcheckers/ui/appl/PlayerLobbyTest.java
@@ -1,4 +1,32 @@
 package com.webcheckers.ui.appl;
 
+import com.webcheckers.appl.PlayerLobby;
+import com.webcheckers.model.Player;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class PlayerLobbyTest {
+    @Test
+    public void testPlayerLobby() {
+        final PlayerLobby lobby = new PlayerLobby();
+        // Invoke test
+        final Player player1 = new Player("Gideon");
+
+        final Player player2 = new Player("Seth");
+
+        lobby.addGamePlayer(player1);
+        lobby.addGamePlayer(player2);
+        ArrayList<Player> list_of_players = lobby.getGamePlayers();
+
+        ArrayList <Player> expected_players = new ArrayList<Player>();
+        expected_players.add(player1);
+        expected_players.add(player2);
+        // Analyze results
+        assertEquals(expected_players, list_of_players);
+    }
 }
