d5612eef31884cacae4ad7b31c559e97694d1d38
Gideon Wikina <widdy49@gmail.com>
2020-10-13 02:49:09 -0400

    Added GetSignInRouteTest

--- src/test/java/com/webcheckers/ui/ui/GetSignInRouteTest.java
+++ src/test/java/com/webcheckers/ui/ui/GetSignInRouteTest.java
@@ -1,4 +1,55 @@
 package com.webcheckers.ui.ui;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.webcheckers.appl.PlayerLobby;
+import com.webcheckers.ui.GetSignInRoute;
+import com.webcheckers.ui.TemplateEngineTester;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import spark.*;
+
+@Tag("UI-tier")
 public class GetSignInRouteTest {
+
+    /**
+     * Parameters used for testing.
+     */
+    private GetSignInRoute CuT;
+    private TemplateEngine templateEngine;
+    private Request request;
+    private Session session;
+    private Response response;
+
+    /**
+     * Initialization.
+     */
+    @BeforeEach
+    private void setup() {
+        request = mock(Request.class);
+        session = mock(Session.class);
+        templateEngine = mock(TemplateEngine.class);
+        Response response = mock(Response.class);
+        PlayerLobby playerLobby = mock(PlayerLobby.class);
+        CuT = new GetSignInRoute(templateEngine);
+    }
+
+    /**
+     * See if the attributes from getSignInRoute works.
+     */
+    @Test
+    public void testAttributes(){
+
+        final TemplateEngineTester tester = new TemplateEngineTester();
+
+        when(templateEngine.render(any(ModelAndView.class) )).thenAnswer(tester.makeAnswer());
+        CuT.handle(request, response);
+
+        tester.assertViewModelExists();
+        tester.assertViewModelIsaMap();
+
+    }
 }
