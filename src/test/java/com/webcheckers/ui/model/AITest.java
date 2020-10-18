96f31860f883df64310a2b773bdefec37effa2c7
Heather Moses <hlm8500@rit.edu>
2020-10-18 18:29:53 -0400

    AI tests

--- src/test/java/com/webcheckers/ui/model/AITest.java
+++ src/test/java/com/webcheckers/ui/model/AITest.java
@@ -1,4 +1,50 @@
 package com.webcheckers.ui.model;
 
+import com.webcheckers.appl.GameCenter;
+import com.webcheckers.model.AI;
+import com.webcheckers.model.Game;
+import com.webcheckers.model.Player;
+import com.webcheckers.model.Position;
+import javafx.geometry.Pos;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Tag("Model-tier")
 public class AITest {
+    AI ai = new AI();
+
+    /**
+     * Test the creation of a new AI.
+     */
+    @Test
+    public void testCreateAPI() {
+        // Analyze results
+        assertNotNull(ai);
+    }
+
+    @Test
+    public void testGetPlayer() {
+        Player cpu = ai.getPlayer();
+        // Analyze results
+        assertEquals(cpu.getName(), "CPU");
+    }
+
+    @Test
+    public void testJumpAverage() {
+        final GameCenter CuT = new GameCenter();
+        // Invoke test
+        // Friendly Player1
+        final Player player1 = new Player("Rocky");
+        // Friendly Player2
+        final Player player2 = new Player("Ella");
+
+        final Game game = CuT.makeGame(player1, player2);
+
+        Position start = new Position(5, 6);
+
+        // Analyze results
+        assertEquals(false, ai.jumpAvailable(game, start));
+    }
 }
