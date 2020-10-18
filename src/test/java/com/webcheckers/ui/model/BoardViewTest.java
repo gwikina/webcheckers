4a48559f681c5ae80fa0e17cd02d7afed6a9381f
Heather Moses <hlm8500@rit.edu>
2020-10-18 18:45:19 -0400

    Board view test

--- src/test/java/com/webcheckers/ui/model/BoardViewTest.java
+++ src/test/java/com/webcheckers/ui/model/BoardViewTest.java
@@ -1,4 +1,25 @@
 package com.webcheckers.ui.model;
 
+import com.webcheckers.model.Board;
+import com.webcheckers.model.Player;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Tag("Model-tier")
 public class BoardViewTest {
+    Player redPlayer = new Player("RED");
+    Player whitePlayer = new Player("WHITE");
+    Board board = new Board(redPlayer, whitePlayer);
+
+    /**
+     * Test the creation of a new BoardView.
+     */
+    @Test
+    public void testCreateBoardView() {
+        // Analyze results
+        assertNotNull(board);
+    }
+    
 }
