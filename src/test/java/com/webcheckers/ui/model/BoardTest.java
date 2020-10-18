6f6a2c2d9e61d7eae9aa6d1f3f35ffcf377a8382
Heather Moses <hlm8500@rit.edu>
2020-10-18 19:09:55 -0400

    Board test

--- src/test/java/com/webcheckers/ui/model/BoardTest.java
+++ src/test/java/com/webcheckers/ui/model/BoardTest.java
@@ -1,7 +1,27 @@
 package com.webcheckers.ui.model;
 
+import com.webcheckers.model.Board;
+import com.webcheckers.model.Player;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @Tag("Model-tier")
 public class BoardTest {
+    Player testPlayer = new Player("Emily");
+    Player testPlayer2 = new Player("Heather");
+    Board board = new Board(testPlayer, testPlayer2);
+
+    /**
+     * Test the creation of a new Board.
+     */
+    @Test
+    public void testCreateBoard() {
+        // Analyze results
+        assertNotNull(board);
+    }
+
+
+
 }
