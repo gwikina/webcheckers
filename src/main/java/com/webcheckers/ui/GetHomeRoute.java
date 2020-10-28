168fbfbb1fc4ab49fbaa72818d578f58ff74ba44
Gideon Wikina <65573460+gwikina@users.noreply.github.com>
2020-10-27 21:27:49 -0400

    Update GetHomeRoute.java

--- src/main/java/com/webcheckers/ui/GetHomeRoute.java
+++ src/main/java/com/webcheckers/ui/GetHomeRoute.java
@@ -1,13 +1,10 @@
 package com.webcheckers.ui;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.logging.Logger;
 
-import com.webcheckers.appl.PlayerLobby;
-import com.webcheckers.model.Player;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -16,15 +13,17 @@ import spark.TemplateEngine;
 
 import com.webcheckers.util.Message;
 
-
+/**
+ * The UI Controller to GET the Home page.
+ *
+ * @author <a href='mailto:bdbvse@rit.edu'>Bryan Basham</a>
+ */
 public class GetHomeRoute implements Route {
   private static final Logger LOG = Logger.getLogger(GetHomeRoute.class.getName());
 
-  public static final Message WELCOME_MSG = Message.info("Welcome to the world of online Checkers.");
+  private static final Message WELCOME_MSG = Message.info("Welcome to the world of online Checkers.");
 
   private final TemplateEngine templateEngine;
-
-  private PlayerLobby lobby;
 
   /**
    * Create the Spark Route (UI controller) to handle all {@code GET /} HTTP requests.
@@ -32,10 +31,10 @@ public class GetHomeRoute implements Route {
    * @param templateEngine
    *   the HTML template rendering engine
    */
-  public GetHomeRoute(final TemplateEngine templateEngine, PlayerLobby lobby) {
+  public GetHomeRoute(final TemplateEngine templateEngine) {
     this.templateEngine = Objects.requireNonNull(templateEngine, "templateEngine is required");
+    //
     LOG.config("GetHomeRoute is initialized.");
-    this.lobby=lobby;
   }
 
   /**
@@ -52,24 +51,12 @@ public class GetHomeRoute implements Route {
   @Override
   public Object handle(Request request, Response response) {
     LOG.finer("GetHomeRoute is invoked.");
-
+    //
     Map<String, Object> vm = new HashMap<>();
     vm.put("title", "Welcome!");
 
     // display a user message in the Home page
     vm.put("message", WELCOME_MSG);
-    Player currentUser = request.session().attribute("currentUser");
-    ArrayList<Player> names = request.session().attribute("names");
-    if (currentUser != null){
-      if (this.lobby.isInGame(currentUser)){
-        response.redirect(WebServer.GAME_URL);
-      }
-
-      vm.remove("message");
-      vm.put("currentUser", currentUser);
-      vm.put("names", names);
-    }
-
 
     // render the View
     return templateEngine.render(new ModelAndView(vm , "home.ftl"));
