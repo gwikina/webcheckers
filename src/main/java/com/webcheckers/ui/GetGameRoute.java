59aab179bb801272a083777b28021378055759ae
Gideon Wikina <65573460+gwikina@users.noreply.github.com>
2020-10-27 21:26:18 -0400

    Update GetGameRoute.java

--- src/main/java/com/webcheckers/ui/GetGameRoute.java
+++ src/main/java/com/webcheckers/ui/GetGameRoute.java
@@ -1,11 +1,9 @@
 package com.webcheckers.ui;
 
 import com.webcheckers.*;
-import com.webcheckers.appl.GameCenter;
 import com.webcheckers.appl.PlayerLobby;
 import com.webcheckers.model.Board;
 import com.webcheckers.model.BoardView;
-import com.webcheckers.model.Game;
 import com.webcheckers.model.Player;
 import com.webcheckers.util.Message;
 import spark.*;
@@ -17,44 +15,54 @@ import java.util.Objects;
 import java.util.logging.Logger;
 
 public class GetGameRoute implements Route {
+    private static final Logger LOG = Logger.getLogger(PostSignInRoute.class.getName());
+
+    private static final Message SIGNIN_MSG = Message.info("sign in");
 
     private final TemplateEngine templateEngine;
 
-    private GameCenter gameCenter;
-
     private PlayerLobby lobby;
 
-    public GetGameRoute(TemplateEngine templateEngine, PlayerLobby lobby, GameCenter gameCenter) {
+    public GetGameRoute(TemplateEngine templateEngine) {
         this.templateEngine = Objects.requireNonNull(templateEngine, "templateEngine is required");
-        this.gameCenter = gameCenter;
-        this.lobby = lobby;
     }
 
     @Override
     public Object handle(Request request, Response response) {
         final Map<String, Object> vm = new HashMap<>();
-        Player currentUser= request.session().attribute("currentUser");
 
-        if (currentUser!=null) {
-            Game game = this.gameCenter.getGame(currentUser);
-            Board board = game.getBoard();
-            BoardView boardView = new BoardView(board, currentUser);
+        Player player1= new Player("Heather");
+        Player currentUser= new Player("John");
+        Board board = new Board(player1, currentUser);
+        BoardView boardView = new BoardView(board, currentUser);
 
-            vm.put("title", "WebCheckers");
-            vm.put("currentUser", currentUser);
-            vm.put("gameID", game.getGameID());
-            vm.put("viewMode", "PLAY");
-            vm.put("redPlayer", game.getRedPlayer());
-            vm.put("whitePlayer", game.getWhitePlayer());
-            vm.put("activeColor", board.getActiveColor());
-            vm.put("board", boardView);
-            return templateEngine.render(new ModelAndView(vm, "game.ftl"));
-        }
-        else{
-            response.redirect(WebServer.HOME_URL);
-            return templateEngine.render(new ModelAndView(vm, "game.ftl"));
-        }
+        Message MSG = Message.info("Please wait for Heather to play");
+        vm.put("currentUser", currentUser);
+        vm.put("title", "WebCheckers");
+        vm.put("gameID", "00000000");
+        vm.put("viewMode", "PLAY");
+        vm.put("redPlayer", player1);
+        vm.put("whitePlayer", currentUser);
+        vm.put("activeColor", "RED");
+        vm.put("board", boardView);
+        vm.put("message", MSG);
+        return templateEngine.render(new ModelAndView(vm, "game.ftl"));
     }
+
+    private void storeCurrentUser(String name, Session session){
+        Player newPlayer = new Player(name);
+        this.lobby.addGamePlayer(newPlayer);
+        session.attribute("currentUser", newPlayer);
+        this.lobby.addUser(newPlayer);
+        ArrayList<Player> names = session.attribute("names");
+        if (names == null) {
+            names = new ArrayList<Player>();
+            session.attribute("names", names);
+        }
+        session.attribute("names", this.lobby.getGamePlayers());
+    }
+
+
 
 
 }
