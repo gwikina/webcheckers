0ef955c3f8ced6802c093ed3c36dd3da8cad67e3
rock <bb5369@g.rit.edu>
2020-10-31 10:21:48 -0400

    Completed end of game route logic

--- src/main/java/com/webcheckers/ui/GetGameRoute.java
+++ src/main/java/com/webcheckers/ui/GetGameRoute.java
@@ -1,5 +1,6 @@
 package com.webcheckers.ui;
 
+import com.google.gson.Gson;
 import com.webcheckers.*;
 import com.webcheckers.appl.GameCenter;
 import com.webcheckers.appl.PlayerLobby;
@@ -39,16 +40,59 @@ public class GetGameRoute implements Route {
             Game game = this.gameCenter.getGame(currentUser);
             Board board = game.getBoard();
             BoardView boardView = new BoardView(board, currentUser);
+            if (!game.isGameOver()) {
+                vm.put("title", "WebCheckers");
+                vm.put("currentUser", currentUser);
+                vm.put("gameID", game.getGameID());
+                vm.put("viewMode", "PLAY");
+                vm.put("redPlayer", game.getRedPlayer());
+                vm.put("whitePlayer", game.getWhitePlayer());
+                vm.put("activeColor", board.getActiveColor());
+                vm.put("board", boardView);
+                return templateEngine.render(new ModelAndView(vm, "game.ftl"));
+            }
+            else{
+                Gson json = new Gson();
+                final Map<String, Object> modeOptions = new HashMap<>(2);
+                modeOptions.put("isGameOver", true);
 
-            vm.put("title", "WebCheckers");
-            vm.put("currentUser", currentUser);
-            vm.put("gameID", game.getGameID());
-            vm.put("viewMode", "PLAY");
-            vm.put("redPlayer", game.getRedPlayer());
-            vm.put("whitePlayer", game.getWhitePlayer());
-            vm.put("activeColor", board.getActiveColor());
-            vm.put("board", boardView);
-            return templateEngine.render(new ModelAndView(vm, "game.ftl"));
+                if (game.getResignPlayer()!=null){
+                   if (currentUser==game.getResignPlayer()) {
+                    modeOptions.put("gameOverMessage", "Resigned Successfully, Exit when ready");
+                    }
+                   else {
+                    modeOptions.put("gameOverMessage", "Congrats You won! Exit when ready");//TODO... all end of game messages
+                    }
+                    vm.put("modeOptionsAsJSON", json.toJson(modeOptions));
+                    vm.put("title", "WebCheckers");
+                    vm.put("currentUser", currentUser);
+                    vm.put("gameID", game.getGameID());
+                    vm.put("viewMode", "PLAY");
+                    vm.put("redPlayer", game.getRedPlayer());
+                    vm.put("whitePlayer", game.getWhitePlayer());
+                    vm.put("activeColor", board.getActiveColor());
+                    vm.put("board", boardView);
+                    return templateEngine.render(new ModelAndView(vm, "game.ftl"));
+                }
+                else {
+                    if (currentUser==game.getWinner()) {
+                        modeOptions.put("gameOverMessage", "Congrats You won by capturing all pieces! Exit when ready");
+                    }
+                    else {
+                        modeOptions.put("gameOverMessage", "You lost... study, and improve your strategy...");//TODO... all end of game messages
+                    }
+                    vm.put("modeOptionsAsJSON", json.toJson(modeOptions));
+                    vm.put("title", "WebCheckers");
+                    vm.put("currentUser", currentUser);
+                    vm.put("gameID", game.getGameID());
+                    vm.put("viewMode", "PLAY");
+                    vm.put("redPlayer", game.getRedPlayer());
+                    vm.put("whitePlayer", game.getWhitePlayer());
+                    vm.put("activeColor", board.getActiveColor());
+                    vm.put("board", boardView);
+                    return templateEngine.render(new ModelAndView(vm, "game.ftl"));
+                }
+            }
         }
         else{
             response.redirect(WebServer.HOME_URL);
